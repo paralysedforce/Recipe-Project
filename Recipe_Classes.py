b398b1de52668abd182243380ca3720347ae0dc1
bcapella123 <briancapella2017@u.northwestern.edu>
2016-03-02 17:06:54 -0500

    Added classes for Recipe Structure

---
+++ Recipe_Classes.py
@@ -0,0 +1,24 @@
+# Master Classes
+
+class recipe:
+	url = ''
+	original_text = ''
+	parsed_text = ''
+	in_list = ingredients[]
+	pr_list = procedure[]
+
+class ingredient:
+	db_link = ''
+	amount = 0
+	amount_unit = ''
+	state = ''
+
+class procedure:
+	db_link = ''
+	in_list = ingredients[]
+	time = ''
+	temp = ''
+
+
+
+
