ed00be5a833a519d771ff33ec1cd85806c946479
Malek Assi <malekassi2212@gmail.com>
2016-03-13 12:46:21 -0500

    handles ingredients not in DB by adding them

--- src/scrape.py
+++ src/scrape.py
@@ -4,11 +4,25 @@ from collections import namedtuple
 import requests
 from bs4 import BeautifulSoup
 
+client = MongoClient()
+
+db = client["k_base"]
+ingredients = db["ingredients"]
+
 def scrape(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     ingredient_strings = soup.findAll("span", {"class": "recipe-ingred_txt",
         "itemprop": "ingredients"})
     step_strings = soup.findAll('span', class_ = "recipe-directions__list--item")
-    return ingredient_strings, step_strings
 
+    for ingredient in ingredients:
+    	cursor = db.ingredients.find("name":ingredient)
+    	i = 0
+    	for document in cursor:
+    		i += 1
+    	if i == 0:
+    		# add to DB
+    		db.ingredients.insert({"name":ingredient})
+
+    return step_strings
