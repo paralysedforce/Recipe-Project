6ea4a3b1c8dc0c469d792977517b2325de816694
Malek Assi <malekassi2212@gmail.com>
2016-03-13 12:15:38 -0500

    fixed classes

--- Recipe_Classes.py
+++ Recipe_Classes.py
@@ -1,28 +1,28 @@
 # Master Classes
 
-class recipe:
-	url = ''
-	name = ''
-	transformation = ''
-	original_text = ''
-	parsed_text = ''
-	in_list = ingredients[]
-	pr_list = procedure[]
+class Recipe:
+	def __init__(self, url='', name='', transformation='', original_text='', parsed_text='', in_list=[], pr_list=[]):
+		self.url = url
+		self.name = name
+		self.transformation = transformation
+		self.original_text = original_text
+		self.parsed_text = parsed_text
+		self.in_list = in_list
+		self.pr_list = pr_list
 
-class ingredient:
-	db_link = ''
-	name = ''
-	amount = 0
-	amount_unit = ''
-	state = ''
+class Ingredient:
+	def __init__(self, name='', amount=0, amount_unit=''):
+		self.name = name
+		self.amount = amount
+		self.amount_unit = amount_unit
 
-class procedure:
-	db_link = ''
-	name = ''
-	in_list = ingredients[]
-	time = ''
-	temp = ''
-	cookware = ''
+class Procedure:
+	def __init__(self, name='', in_list=[], time='', temp='', cookware=[]):
+		self.name = ''
+		self.in_list = []
+		self.time = ''
+		self.temp = ''
+		self.cookware = []
 
 
 
