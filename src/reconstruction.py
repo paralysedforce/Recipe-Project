11319d231b2666e6bde3f7de871457680e010a86
Vyas Alwar <vyasalwar@gmail.com>
2016-03-13 17:03:01 -0500

    Made printing less trashy

--- src/reconstruction.py
+++ src/reconstruction.py
@@ -10,33 +10,39 @@ import transform
 # procedure text
 
 def reconstruct(r):
-	fs = ''
-	fs += r.name + '    ' + 'transformed to fit the following criteria: ' + r.transformation + '\n\nIngredients:\n'
-	for ing in r.in_list:
-		line = '\t-' + ing.name + '  ' + str(ing.amount) + ' ' + ing.amount_unit +'\n'
-		fs += line
-	fs += '\nSteps:\n'
-	tr = ['Next', 'Now', 'After that']
-	i = 1
-	j = 0
-	for proc in r.pr_list:
-		line = ''
-		if i==1:
-			line = 'First, '
-		elif i == len(r.pr_list):
-			line = 'Finally, '
-		else:
-			if j>=len(tr):
-				j = 0
-			line += tr[j]
-			j+=1
-		if transform.is_cooking_method(proc):
-			line += 'with ' + ''.join([i_ + ' and ' for i_ in proc.cookware]) + ',' + proc.name + ' '.join([i_ + ' and ' for i_ in proc.in_list]) + ' for ' + proc.time + ' at ' + proc.temp + '.'
-		else:
-			line += proc.name + ' ' + ''.join([i_ + ' and ' for i_ in proc.in_list]) + '.'
-		fs += line + '\n'
-		i += 1
-	print str(fs)
+    fs = ''
 
+    fs += '%s\ttransformed to fit the following criteria: %s' % (r.name, r.transformation)
+    fs += "\n\nIngredients:\n"
+    for ing in r.in_list:
+        fs += '\t-%s  %s %s\n' % (ing.name, str(ing.amount), ing.amount_unit)
+    fs += '\nSteps:\n'
+    transitions = ['Next, ', 'Now, ', 'After that, ']
+    step_count = 1
+    j = 0
+    for proc in r.pr_list:
+        line = ''
+        if step_count == 1:
+            line = 'First, '
+        elif step_count == len(r.pr_list):
+		    line = 'Finally, '
+        else:
+            line += transitions[step_count % len(transitions)]
 
-		
+        if transform.is_cooking_method(proc):
+            if proc.cookware:
+                line += 'with ' 
+                line += ' and cookware: '.join(cookware for cookware  in proc.cookware) + ',' 
+            line += proc.name + ' '
+            if proc.in_list:
+                line += ', '.join(i for i in proc.in_list[:-1]) + ', and %s ' % proc.in_list[-1]
+            line += 'for {time} at {temp}.'.format(time=proc.time, temp=proc.temp)
+        else:
+            line += proc.name 
+            if proc.in_list:
+                line += ' ' + ', '.join(ing for ing in proc.in_list) 
+            line += '.'
+
+        fs += line + '\n'
+        step_count += 1
+    print str(fs)
