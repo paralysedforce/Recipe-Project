cb6256eefb46ca9232140f9b71cabc89f8198bb7
svorbrich <svorbrich@gmail.com>
2016-03-13 14:41:19 -0500

    Added whole fully-functional (theoretically) text user interface. Also fixed several syntax errors in other files.

---
+++ src/ui.py
@@ -0,0 +1,47 @@
+import recipe_classes
+import parser
+import transform
+
+def main():
+    """This function runs the user interface, prompting the user and calling the necessary functions to resolve their queries"""
+
+    print "Hello, welcome to the recipe transformer!"
+    print "To get started, please enter the URL of the recipe you would like to transform. At any point, you may enter \"back\" to return to the previous menu."
+    print "You may also enter \"done\" at any point to terminate the program."
+    menu = 0
+    prompt = ["Enter AllRecipes.com URL:  ", "Enter transformation (enter \"list\" for a list of posible transformations):  ", ]
+    URL = ""
+
+    transformations = ['pescetarian', 'vegetarian', 'east asian', 'italian', 'easy', 'low sodium', 'low carb']
+
+
+    print "Hello world"
+
+    while(True):
+        command = raw_input(prompt[menu]).lower()
+        if command == "done":
+            return
+        if command == "back":
+            menu -= 1
+            if menu < 0:
+                menu = 0
+            continue
+        if menu == 0:
+            if command[:30] == "http://allrecipes.com/recipes/":
+                URL = command
+                menu += 1
+                continue
+            print "Invalid URL"
+        if menu >= 1:
+            if command == "list":
+                print ", ".join(transformations)
+                continue
+            if command in transformations:
+                recipe = recipe_classes.Recipe(URL, "recipe", command)
+                transformed_recipe = parser.main(recipe)
+                print transformed_recipe
+            print "Invalid transformation"
+
+
+if __name__ == '__main__':
+    main()
