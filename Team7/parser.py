9dc1a67b2228b3be2549d1e31365efd93503bbc6
Vyas Alwar <vyasalwar@gmail.com>
2016-03-15 09:20:20 -0500

    removed extraneous files from Team7

--- Team7/parser.py
+++ Team7/parser.py
@@ -1,30 +1,15 @@
 #!usr/bin/python
 
-from data import *
-
-import scrape
 from string import punctuation
 import re
+from collections import namedtuple
+import nltk.corpus
 
-import nltk
-from pymongo import MongoClient
-import recipe_classes
-import reconstruction
-import transform
+from data import *
+import scrape
 
-client = MongoClient()
-
-db = client["k_base"]
-ingredients = db["ingredients"]
-procedures = db["procedures"]
-transformations = db["transformations"]
-
-# Quantity = namedtuple("Quantity", ["value", "unit"])
-# Ingredient = namedtuple("Ingredient", ['name', 'quantity', 'descriptors'])
-# Step = namedtuple("Step", ["ingredients", "processes", "cookware"])
-
-#TODO: unit tests for recipes
-#TODO: improve recognize_descriptors
+Quantity = namedtuple('Quantity', ['amount', 'unit'])
+Ingredient = namedtuple('Ingredient', ['name', 'quantity', 'descriptors'])
 
 def parse_ingredient(ingredients):
     """ingredients is a string scraped from the web site. This function
@@ -32,9 +17,10 @@ def parse_ingredient(ingredients):
     number = recognize_number(ingredients)
     unit = recognize_unit(ingredients)
     ingredient_name = recognize_ingredient(ingredients)
-    # descriptors = recognize_descriptors(ingredients)
-
-    ingredient = recipe_classes.Ingredient(ingredient_name, number, unit)
+    descriptors = recognize_descriptors(ingredients)
+    
+    quantity = Quantity(number, unit)
+    ingredient = Ingredient(ingredient_name, quantity, descriptors)
     return ingredient
 
 def recognize_number(ingredients):
@@ -57,7 +43,7 @@ def recognize_unit(ingredients):
         for variation in variations:
             if variation in ingredients:
                 return unit
-    return 'unit'
+    return 'discrete'
 
 def recognize_ingredient(ingredients):
     # preprocessing
@@ -78,23 +64,13 @@ def recognize_descriptors(ingredients, data = None):
         no_units = all([stripped not in UNITS[unit] for unit in UNITS])
         if no_numerals and no_stopwords and no_ingredients and no_units:
             descriptors.append(word.lower())
+    return descriptors
 
 # Step parsing
 def parse_step(step):
     step_procedures = []
     step_ingredients = []
     step_cookware = []
-
-    # for direction in DIRECTIONS:
-    #     if direction in step:
-    #         step_directions.append(direction)
-    # for ingredient in ingredients:
-    #     if ingredient.name in step:
-    #         step_ingredients.append(ingredient.name)
-    # for cookware in COOKWARE:
-    #     if cookware in step:
-    #         step_cookware.append(cookware)
-    # return Step(step_ingredients, step_directions, step_cookware)
 
     ing_cursor = db.ingredients.find()
     proc_cursor = db.procedures.find()
@@ -111,94 +87,38 @@ def parse_step(step):
             step_cookware.append(document['name'])
 
 
-    #GET TIME AND TEMP FROM STEP
-    time = '100 minutes'
-    temp = '100F'
+    time = recognize_time(step) 
+    temp = recognize_temp(step)
     if not step_procedures:
         step_procedures = ['placeholder_process']
     proc = recipe_classes.Procedure(step_procedures[0], step_ingredients, step_cookware, time, temp)
     return proc
 
-# def recognize_time(step):
-# def recognize_temp(step):
+def recognize_time(step):
+    processed_step = _strip_punctuation(step.lower()).split()
+    for i in xrange(len(processed_step)):
+        word = processed_step[i]
+        if word in TIME and i > 0:
+            prev_word = processed_step[i-1] 
+            if all(map(lambda c: c in '1234567890' or c in punctuation,
+                prev_word)):
+                return prev_word + ' ' +  word
 
-## Helper
+def recognize_temp(step):
+    lower_step = step.lower()
+    if 'degrees' in lower_step:
+        ind = lower_step.split().index('degrees')
+        return " ".join(i for i in step.split()[ind-1: ind+2])
+    elif 'low heat' in lower_step:
+        return 'low heat'
+    elif 'medium heat' in lower_step:
+        return 'medium heat'
+    elif 'high heat' in lower_step:
+        return 'high heat'
+    return ''
+
+               
 def _strip_punctuation(string):
     return "".join(char for char in string if char not in punctuation)
 
-def main(original_recipe):
-    # urls = ['http://allrecipes.com/recipe/easy-meatloaf/',
-    #         'http://allrecipes.com/Recipe/Easy-Garlic-Broiled-Chicken/',
-    #         'http://allrecipes.com/Recipe/Baked-Lemon-Chicken-with-Mushroom-Sauce/',
-    #         'http://allrecipes.com/Recipe/Meatball-Nirvana/']
-    scraped_info = scrape.scrape(original_recipe.url)
-    scraped_ing = scraped_info[0]
-    scraped_steps = scraped_info[1]
 
-    # parse ingredient info, create objects
-    ingredients = []
-    for ingredient in scraped_ing:
-        ing_info = ingredient.contents
-        new_ing = parse_ingredient(ing_info[0])
-        cursor = db.ingredients.find({"name":new_ing.name})
-        i = 0
-        for document in cursor:
-            i += 1
-        if i == 0:
-            # add to DB
-            db.ingredients.insert({"name":new_ing.name})
-        ingredients.append(new_ing)
-
-    steps = []
-    for step in scraped_steps:
-        step_info = step.contents
-        if not step_info:
-            continue # HANDLE EMPTY
-        new_proc = parse_step(step_info[0])
-        steps.append(new_proc)
-
-    # original_recipe.parsed_text = steps
-    # all_ing = []
-    # all_proc = []
-    # i = 0
-    # for step in steps:
-    #     print step
-
-    #     step_tuple = parse_step(step)
-    #     ingredients = step_tuple[0]
-    #     procedures = step_tuple[1]
-    #     cookware = step_tuple[2]
-    #     step_ing = []
-    #     step_proc = []
-    #     step_cw = []
-
-    #     for ingredient in ingredients:
-    #         step_ing.append(Ingredient(ingredient)) #get amounts?
-    #     for cw in cookware:
-    #         step_cw.append(cw)
-    #     for proc in procedures:
-    #         step_proc.append(Procedure(proc, step_ing, step_cw)) #get time/temp?
-
-    #     all_ing[i] = step_ing
-    #     all_proc[i] = step_procedures
-    #     i += 1
-
-    original_recipe.in_list = ingredients
-    original_recipe.pr_list = steps
-
-    #call transform etc
-    try:
-        transformed_recipe = transform.transform(original_recipe)
-    except RuntimeError:
-        print e
-        return original_recipe, Recipe()
-
-    print transformed_recipe
-    reconstruction.reconstruct(transformed_recipe)
-    return original_recipe, transformed_recipe
-
-
-
-
-# if __name__ == "__main__":
-#     main()
